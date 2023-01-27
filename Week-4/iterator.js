6c56f108217412c382b93dae34d696b8f8519214
AkashLiquiLoans <akashpathadeakol@gmail.com>
2023-01-27 21:22:30 +0530

    Exercise 4 iterator, promise, prototype

---
+++ Week-4/iterator.js
@@ -0,0 +1,27 @@
+/**
+ * Exercise 4.3
+ * 
+ * Implement Fibonacci Series with Iterators
+*/
+
+const Fib = (n) => ({
+
+    [Symbol.iterator]: () => {
+        let i = 1;
+        let thisNum = 0, nextNum = 0;
+        return {
+            next: () => {
+                i++;
+                if(i > n){
+                    return { done: true }
+                }
+                thisNum = nextNum;
+                nextNum = (thisNum + nextNum) || 1;
+                // [thisNum, nextNum] = [nextNum, (thisNum + nextNum) || 1];
+                return { value: thisNum, done: false }
+            }
+        }
+    }
+})
+
+console.log([...Fib(5)]);
