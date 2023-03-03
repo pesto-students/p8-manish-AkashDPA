91e4d99304eaa61baf69746bf1108b22660af7ea
AkashLiquiLoans <akashpathadeakol@gmail.com>
2023-03-04 04:05:42 +0530

    week-10 traverse & validateBTree

--- Week-10/validateBTree.js
+++ Week-10/validateBTree.js
@@ -5,3 +5,36 @@
  * Theright subtree of a node contains only nodes with keys greater than the node's key. 
  * Both the left and right subtrees must also be binary search trees
  */
+
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+}
+  
+let root = new Node(2);
+root.left = new Node(1);
+root.right = new Node(3);
+  
+function isBST() {
+    return checkBST(root, -Infinity, Infinity);
+}
+  
+function checkBST(root, min, max) {
+    if (root === null) {
+      return true;
+    }
+  
+    if (root.value < min || root.value > max) {
+      return false;
+    }
+  
+    return (
+      checkBST(root.left, min, root.value - 1) &&
+      checkBST(root.right, root.value + 1, max)
+    );
+}
+
+console.log(isBST());
